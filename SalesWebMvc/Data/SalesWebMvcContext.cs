b9c7e9d0d4df2d769a80b6531cbf47ff5bd8fc42
uniedit <buraconegroquasar@gmail.com>
2025-11-27 16:44:58 -0300

    MySql adaptation and first migration

--- SalesWebMvc/Data/SalesWebMvcContext.cs
+++ SalesWebMvc/Data/SalesWebMvcContext.cs
@@ -1,4 +1,14 @@
-﻿namespace SalesWebMvc.Data {
-    public class SalesWebMvcContext {
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace SalesWebMvc.Models {
+    public class SalesWebMvcContext : DbContext {
+        public SalesWebMvcContext(DbContextOptions<SalesWebMvcContext> options)
+            : base(options) {
+        }
+        public DbSet<SalesWebMvc.Models.Department> Department { get; set; }
     }
 }
