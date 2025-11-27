b786f22d43e39e2fec225e83b92cd267b1cec2d1
uniedit <buraconegroquasar@gmail.com>
2025-11-27 16:23:55 -0300

    CRUD Scaffolding without Scaffolding

--- SalesWebMvc/Controllers/DepartmentsController.cs
+++ SalesWebMvc/Controllers/DepartmentsController.cs
@@ -1,16 +1,4 @@
-﻿using SalesWebMvc.Models;
-using Microsoft.AspNetCore.Mvc;
-using System.Collections.Generic;
-
-namespace SalesWebMvc.Controllers {
-    public class DepartmentsController : Controller {
-        public IActionResult Index() {
-
-            List<Department> list = new List<Department>();
-            list.Add(new Department { Id = 1, Name = "Eletronics" });
-            list.Add(new Department { Id = 2, Name = "Fashion" });
-
-            return View(list);
-        }
+﻿namespace SalesWebMvc.Controllers {
+    public class DepartmentsController {
     }
 }
---
+++ SalesWebMvc/Data/SalesWebMvcContext.cs
@@ -0,0 +1,4 @@
+﻿namespace SalesWebMvc.Data {
+    public class SalesWebMvcContext {
+    }
+}
